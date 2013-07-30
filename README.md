38060d1ed7367f44a7a14a8785a27ffb54cc9d02
johnpaulh <johnpaul@freshdesk.com>
2013-07-30 13:02:55 +0530

    Freshdesk API: Ruby samples program.
    Some sample programs in ruby to create ticket, contact.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Ruby Sample Programs to use freshdesk api
-====================================
+Ruby Sample Programs to use freshdesk api with RestClient
+=========================================================
 1. Create ticket api with cc_emails,custom_fields attribute
+2. Create ticket with attachments
+3. Create user basic
+4. Create user along with avatar image.
+5. Create user using(Net::Http)
---
+++ ticket_create_json.rb
@@ -0,0 +1,12 @@
+#@author : John Paul.H
+#Create a ticket with custom fields, cc_emails attributes.
+require "rubygems"
+require "rest_client"
+require "json"
+
+site = RestClient::Resource.new("http://yourcompany.domain.com/helpdesk/tickets.json","sample@freshdesk.com","test")
+
+response = site.post({:helpdesk_ticket=>{:description=>"Test ticket creation with attachments",:subject=>"new ticket sample",:email=>"test@abc.com",:custom_field=>{:license_1=>"ABCDEF"}},:cc_emails=>"myemail@gmail.com,test@gmail.com"},:content_type=>"application/json")
+
+puts "response: #{response.code} \n #{response.body}"
+
